4a8bf021684460ef6a0a4bed875a406d1ac231f1
Timothy Aveni <timothy@tacosareawesome.com>
2015-01-13 20:08:35 -0500

    Add Vesuvius metronome the Frank Ticheli piece the web audio API is working a lot worse than what I had been doing before, which is really frustrating. Not sure how I should fix it. Maybe I'm queueing too many sounds at once or something... it makes bad noises and it sometimes waits the wrong amount of time between measures.

---
+++ vesuvius/index.js
@@ -0,0 +1,90 @@
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var audioSoundBuffers = {
+	"highClick": null,
+	"lowClick":  null,
+	"subClick":  null
+};
+
+var context = new AudioContext();
+
+var setupSlider = function () {
+	var bpmSlider = document.getElementById("bpmSlider");
+	var bpmEntry = document.getElementById("bpmEntry");
+	bpmSlider.onchange = bpmSlider.onmousemove = function () {
+		bpmEntry.value = bpmSlider.value;
+	};
+
+	bpmEntry.onchange = bpmEntry.onkeyup = bpmEntry.onclick = function () {
+		bpmSlider.value = bpmEntry.value;
+	};
+};
+
+var loadBufferedSounds = function (callback) {
+	TOTAL_SOUNDS = 3;
+	loaded       = 0;
+
+	[["highClick", "audio/highClick.wav"],
+		["lowClick", "audio/lowClick.wav"],
+		["subClick", "audio/subClick.wav"]].forEach(function (click) {
+		var request = new XMLHttpRequest();
+		request.open("GET", click[1], true);
+		request.responseType = "arraybuffer";
+
+		request.onload = function () {
+			context.decodeAudioData(request.response, function (buffer) {
+				audioSoundBuffers[click[0]] = buffer;
+				loaded++;
+				if (loaded == TOTAL_SOUNDS) {
+					callback();
+				}
+			}, function () {
+				// TODO: sound load error
+			});
+		};
+
+		request.send();
+	});
+};
+
+var startMetronome = function () {
+	var bpm = document.getElementById("bpmSlider").value;
+	var currentTime = context.currentTime;
+
+	vesuviusMeasures.forEach(function (measureType) {
+		var beatArray = measureTypes[measureType];
+
+		for (var i = 0; i < beatArray.length; i++) {
+			var source = context.createBufferSource();
+			switch (beatArray[i]) {
+				case 0:
+					source.buffer = audioSoundBuffers.highClick;
+					break;
+				case 1:
+					source.buffer = audioSoundBuffers.lowClick;
+					break;
+				case 2:
+					source.buffer = audioSoundBuffers.subClick;
+					break;
+			}
+			source.connect(context.destination);
+			source.start(currentTime);
+
+			currentTime += 60 / bpm;
+		}
+	});
+
+	//console.log("done");
+};
+
+window.onload = function () {
+	setupSlider();
+
+	loadBufferedSounds(function () {
+		var startButton = document.getElementById("startButton")
+		startButton.disabled = "";
+		startButton.onclick = function () {
+			startMetronome();
+		};
+	});
+};
---
+++ vesuvius/vesuvius.js
@@ -0,0 +1,114 @@
+var vesuviusMeasures = [];
+
+// 0: 1/4
+// 1: 2/4
+// 2: 3/4
+// 3: 4/4
+// 4: 3/2
+// 5: 7/8
+// 6: 8/8
+// 7: 9/8 (2-3-2-2)
+// 8: 9/8 (2-2-3-2)
+// 9: 11/8
+
+for (var i = 0; i < 46; i++)
+	vesuviusMeasures.push(3);
+
+for (var i = 0; i < 7; i++)
+	vesuviusMeasures.push(7);
+
+for (var i = 0; i < 9; i++)
+	vesuviusMeasures.push(3);
+
+for (var i = 0; i < 7; i++)
+	vesuviusMeasures.push(6);
+
+for (var i = 0; i < 4; i++)
+	vesuviusMeasures.push(3);
+
+for (var i = 0; i < 12; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(2);
+
+for (var i = 0; i < 3; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(1);
+
+for (var i = 0; i < 3; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(0);
+
+for (var i = 0; i < 3; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(9);
+
+for (var i = 0; i < 15; i++)
+	vesuviusMeasures.push(3);
+
+for (var i = 0; i < 42; i++)		// rit marked
+	vesuviusMeasures.push(2);
+
+vesuviusMeasures.push(4);
+
+for (var i = 0; i < 14; i++)
+	vesuviusMeasures.push(2);
+
+vesuviusMeasures.push(4);
+
+for (var i = 0; i < 34; i++)
+	vesuviusMeasures.push(2);
+
+vesuviusMeasures.push(4);
+
+for (var i = 0; i < 33; i++)
+	vesuviusMeasures.push(2);
+
+vesuviusMeasures.push(3);
+vesuviusMeasures.push(7);
+vesuviusMeasures.push(2);
+
+for (var i = 0; i < 2; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(2);
+
+for (var i = 0; i < 24; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(0);
+
+vesuviusMeasures.push(7);
+vesuviusMeasures.push(7);
+vesuviusMeasures.push(8);
+vesuviusMeasures.push(7);
+
+for (var i = 0; i < 5; i++)
+	vesuviusMeasures.push(3);
+
+for (var i = 0; i < 56; i++)
+	vesuviusMeasures.push(2);
+
+for (var i = 0; i < 28; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(2);
+
+for (var i = 0; i < 2; i++)
+	vesuviusMeasures.push(7);
+
+vesuviusMeasures.push(2);
+
+for (var i = 0; i < 9; i++)
+	vesuviusMeasures.push(7);
+
+for (var i = 0; i < 10; i++)
+	vesuviusMeasures.push(1);
+
+for (var i = 0; i < 2; i++)
+	vesuviusMeasures.push(5);
+
+// assert vesuviusMeasures.length == 388
